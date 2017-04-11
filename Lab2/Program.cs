e411b294a97b97069c09294ec8aa40e71094a714
Lanna <Lanna_lmb@hotmail.com>
2017-04-11 14:53:03 -0400

    Added Most If-Else Statements

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -18,16 +18,40 @@ namespace Lab2
 
             // Input
             Console.WriteLine("Enter a Number Between 1 and 100: ");
+            Console.WriteLine();
 
 
             // Validation
             while (!int.TryParse(Console.ReadLine(), out input))
             {
-                Console.WriteLine("Please Enter a Number Between 1 and 100: ");
+                Console.WriteLine("Please Enter a Number Between 1 and 100:   ");
+                Console.WriteLine();
             }
 
 
-            if (input % 2)
+            // Spacing
+            Console.WriteLine();
+
+
+            // Process
+            if (input % 2 != 0)
+            {
+
+                Console.WriteLine("The Number " + input + " is Odd.");
+            }
+
+            else if (input % 2 == 0 && input >= 2 && input <= 25)
+            {
+
+                Console.WriteLine("The Number " + input + " is Even and Less Than 25.");
+            }
+
+            else if (input % 2 == 0 && input >= 26 && input <= 60)
+            {
+
+                Console.WriteLine("The Number " + input + " is Even.");
+            }
+
         }
     }
 }
