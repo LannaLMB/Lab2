1fa7c190237daec9b915dabdf7290b9af6e00c28
Lanna <Lanna_lmb@hotmail.com>
2017-04-11 15:41:06 -0400

    Finished Product - Code Complete

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -11,7 +11,7 @@ namespace Lab2
             // Declare Variables
             int input;
             string name;
-           
+            string Continue;
 
 
             // Name Input
@@ -20,58 +20,83 @@ namespace Lab2
             Console.WriteLine();
 
 
-            // Number Input
-            Console.WriteLine(name + ", Enter a Number Between 1 and 100: ");
-            Console.WriteLine();
-
-
-            // Number Validation
-            while (!int.TryParse(Console.ReadLine(), out input))
+            // Loop if User Chooses to Continue
+            while (true)
             {
-                Console.WriteLine(name + ", Please Enter a Number Between 1 and 100:   ");
+
+                // Number Input
+                Console.WriteLine(name + ", Enter a Number Between 1 and 100: ");
                 Console.WriteLine();
-            }
 
 
-            // Spacing
-            Console.WriteLine();
+                // Number Validation
+                while (!int.TryParse(Console.ReadLine(), out input))
+                {
+                    Console.WriteLine(name + ", Please Enter a Number Between 1 and 100:   ");
+                    Console.WriteLine();
+                }
 
 
-            // Process
-            //if (input > 100 || input < 1)
-            //{
-            //    Console.WriteLine("Please Enter a Valid Number Between 1 and 100");
-            //}
-            if (input % 2 != 0)
+                // Spacing
+                Console.WriteLine();
+
+
+                // Process
+                if (input % 2 != 0)
+                {
+
+                    Console.WriteLine("The Number " + input + " is Odd.");
+                }
+
+                else if (input % 2 == 0 && input >= 2 && input <= 25)
+                {
+
+                    Console.WriteLine("The Number " + input + " is Even and Less Than 25.");
+                }
+
+                else if (input % 2 == 0 && input >= 26 && input <= 60)
+                {
+
+                    Console.WriteLine("The Number " + input + " is Even.");
+                }
+
+                else if (input % 2 == 0 && input > 60 && input <= 100)
+                {
+
+                    Console.WriteLine("The Number " + input + " is Even.");
+                }
+
+                else if (input % 2 != 0 && input > 60 && input <= 100)
+                {
+
+                    Console.WriteLine("The Number " + input + " is Odd.");
+                }
+                else
+                    Console.WriteLine("You Must Enter a Number Between 1 and 100");
+
+
+
+            // Continue Loop
+            while (true)
             {
+                Console.WriteLine("");
+                Console.WriteLine("Would You Like to Continue " + name + " (y/n)");
+                Continue = Console.ReadLine().ToUpper();
+                Console.WriteLine("");
 
-                Console.WriteLine("The Number " + input + " is Odd.");
+                if (Continue == "Y")
+                    break;
+
+                if (Continue == "N")
+                {
+                    Console.WriteLine("Bye, " + name + "!");
+                    return;
+                }
+
+                else
+                    Console.WriteLine("Please Enter Y or N");
+                }
             }
-
-            else if (input % 2 == 0 && input >= 2 && input <= 25)
-            {
-
-                Console.WriteLine("The Number " + input + " is Even and Less Than 25.");
-            }
-
-            else if (input % 2 == 0 && input >= 26 && input <= 60)
-            {
-
-                Console.WriteLine("The Number " + input + " is Even.");
-            }
-
-            else if (input % 2 == 0 && input > 60)
-            {
-
-                Console.WriteLine("The Number " + input + " is Even.");
-            }
-
-            else if (input % 2 != 0 && input > 60)
-            {
-
-                Console.WriteLine("The Number " + input + " is Odd.");
-            }
-
         }
     }
 }
