471e0d0c1d9355993f1b4bef9cc9b4ab34bec641
Lanna <Lanna_lmb@hotmail.com>
2017-04-11 14:27:46 -0400

    Initial Commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Declare Variables
+            int input;
+
+
+            // Input
+            Console.WriteLine("Enter a Number Between 1 and 100: ");
+
+
+            // Validation
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("Please Enter a Number Between 1 and 100: ");
+            }
+
+
+            if (input % 2)
+        }
+    }
+}
